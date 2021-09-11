ddbe196b5088d806495a5aa0af67062237081694
Emanuel <emanuelsalasdeleon@hotmail.com>
2021-09-11 15:07:04 -0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { QuotesScreen } from './QuotesScreen';
-
-ReactDOM.render(
-  <QuotesScreen/>,
-  document.getElementById('root')
-);
